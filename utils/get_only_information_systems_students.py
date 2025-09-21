c4c4dea71a0c204a3de8860c1412857a64a8f8a2
Raisson Souza <raissonrai@gmail.com>
2025-09-21 19:31:03 -0300

    Implementados algoritmos Random Forest e Gradient Boosting.

--- utils/get_only_information_systems_students.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def get_only_information_systems_students(df: pd.DataFrame) -> pd.DataFrame:
-    """Filtra apenas registros de alunos de sistemas de informação."""
-    return df[df["course"] == "Sistemas de Informação"]
--- utils/treat_dataset.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def treat_dataset(df: pd.DataFrame) -> pd.DataFrame:
-    """Colunas não utilizadas serão removidas."""
-    return df.drop(columns=["birthdate", "sex", "city", "week_day"])
